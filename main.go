db7f0d9e7be681f26a903f17527cb00d5798995d
Lev E <bearnode@gmail.com>
2019-12-17 11:17:24 +0300

    remove todos

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import "time"
 
 func main() {
-	// TODO: Remove formatting logic from data collectors
-	// TODO: Add formatting logic to logger
-	// TODO: Add README
 	for {
 		logger()
 		time.Sleep(time.Minute * 5)
