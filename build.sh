5b53b66377514d345a4617dc8fdc48b1e7224392
Lev E <bearnode@gmail.com>
2019-12-23 10:49:29 +0300

    update filename

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
-filename="pi-temp-$tag-arm64-linux"
+filename="pi-stats-$tag-arm64-linux"
 
-env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-temp cmd/main.go &&
-    cd release && tar -czvf $filename.tar.gz pi-temp
+env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-stats cmd/main.go &&
+    cd release && tar -czvf $filename.tar.gz pi-stats
