7a82c3771c4e7244f52a5106cfc21b39ab8a3cf1
Lev E <bearnode@gmail.com>
2020-01-13 19:17:18 +0300

    update build script, gitignore
    Signed-off-by: Lev E <bearnode@gmail.com>

--- build_example.sh
+++ build_example.sh
@@ -3,5 +3,5 @@
 tag="$(git describe --tags $(git rev-list --tags --max-count=1))"
 filename="pi-stats-$tag-arm64-linux"
 
-env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-stats example/main.go &&
+env GOOS=linux GOARCH=arm GOARM=6 go build -o release/pi-stats example/*.go &&
     cd release && tar -czvf $filename.tar.gz pi-stats
